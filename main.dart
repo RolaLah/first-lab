8fca72643d02bdcf6ea29e33c9a031507e984205
RolaLah <rolalahloub6@gmail.com>
2022-08-25 18:05:52 +0300

    3rd update

--- main.dart
+++ main.dart
@@ -1,5 +1,14 @@
 void main() {
   String my_name = "Rola";
 
-  print(my_name);
+  Map rolaInformation = {
+    "First_Name": "Rola",
+    "Last_Name": "Lahloub",
+    "Age": 23,
+    "Iducation_Info": {
+      "Uni_Name": "Al-Isra universty",
+      "Faculty": "IT",
+      "Graduation_year": "2022"
+    }
+  };
 }
